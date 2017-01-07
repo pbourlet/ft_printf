d881884784cfd0984f9f62acd09d81aad104b799
Pierre-emmanuel BOURLET <pbourlet@e3r10p11.42.fr>
2017-01-07 16:57:57 +0100

    itoabase

--- libft/ft_itoabase.c
+++ libft/ft_itoabase.c
@@ -5,9 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pbourlet <pbourlet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/13 10:49:58 by pbourlet          #+#    #+#             */
-/*   Updated: 2016/12/13 10:50:00 by pbourlet         ###   ########.fr       */
+/*   Created: 2017/01/07 16:49:00 by pbourlet          #+#    #+#             */
+/*   Updated: 2017/01/07 16:55:08 by pbourlet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_itoabase(unsigned long nb, int base)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!(str = ft_strnew(11)))
+		return (NULL);
+	if (nb == 0)
+		return (ft_strcpy(str, "0"));
+	while (nb != 0)
+	{
+		if (nb % base <= base - 1)
+			str[i] = (nb % base) + '0';
+		if (nb % base >= 10)
+			str[i] = 'a' + ((nb % base) % 10);
+		nb = nb / base;
+		i++;
+	}
+	str[i] = '\0';
+	str = ft_strrev(str);
+	return (str);
+}
+
+int		main(void)
+{
+	int i = 42;
+
+	ft_putstr(ft_itoabase(i, 16));
+	return (0);
+}
